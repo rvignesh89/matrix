43fb6b7876bbd5794bd6f311dca73521bdeb43a7
rvignesh <rvignesh@outlook.com>
2015-11-13 00:32:34 +0800

    add port into config

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,14 +5,16 @@ require 'json'
 class Book
   @@api_url = ENV['API_URL'] + 'book/'
   @@api_host = ENV['API_HOST']
+  @@api_port = ENV['API_PORT']
   def self.all()
+    puts @@api_url
     return JSON.parse(Net::HTTP.get_response(URI(@@api_url)).body)["data"]
   end
   def self.new(book)
     req = Net::HTTP::Post.new('/book', initheader = {'Content-Type' =>'application/json'})
     book[:status] = 'Available'
     req.body = book.to_json
-    response = Net::HTTP.new(@@api_host, 5000).start {|http| http.request(req) }
+    response = Net::HTTP.new(@@api_host, @@api_port).start {|http| http.request(req) }
   end
   def self.find(name)
     return JSON.parse(Net::HTTP.get_response(URI(@@api_url+name.gsub(' ','%20'))).body)["data"][0]
@@ -21,6 +23,6 @@ class Book
     path = '/book/'+name.gsub(' ','%20')
     req = Net::HTTP::Put.new(path, initheader = {'Content-Type' =>'application/json'})
     req.body = updatedBook.to_json
-    response = Net::HTTP.new(@@api_host, 5000).start {|http| http.request(req) }
+    response = Net::HTTP.new(@@api_host, @@api_port).start {|http| http.request(req) }
   end
 end
