e2bc9e5937c5c754825427306c8778c929cecb08
rvignesh <rvignesh@outlook.com>
2015-11-12 23:31:37 +0800

    read api url from env var

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,24 +3,24 @@ require 'rubygems'
 require 'json'
 
 class Book
+  @@api_url = ENV['API_URL'] + 'book/'
+  @@api_host = ENV['API_HOST']
   def self.all()
-    return JSON.parse(Net::HTTP.get_response(URI('http://localhost:5000/book/')).body)["data"]
+    return JSON.parse(Net::HTTP.get_response(URI(@@api_url)).body)["data"]
   end
   def self.new(book)
     req = Net::HTTP::Post.new('/book', initheader = {'Content-Type' =>'application/json'})
     book[:status] = 'Available'
     req.body = book.to_json
-    response = Net::HTTP.new("localhost", 5000).start {|http| http.request(req) }
+    response = Net::HTTP.new(@@api_host, 5000).start {|http| http.request(req) }
   end
   def self.find(name)
-    return JSON.parse(Net::HTTP.get_response(URI('http://localhost:5000/book/'+name.gsub(' ','%20'))).body)["data"][0]
+    return JSON.parse(Net::HTTP.get_response(URI(@@api_url+name.gsub(' ','%20'))).body)["data"][0]
   end
   def self.update(name,updatedBook)
     path = '/book/'+name.gsub(' ','%20')
-    puts path
-    puts updatedBook
     req = Net::HTTP::Put.new(path, initheader = {'Content-Type' =>'application/json'})
     req.body = updatedBook.to_json
-    response = Net::HTTP.new("localhost", 5000).start {|http| http.request(req) }
+    response = Net::HTTP.new(@@api_host, 5000).start {|http| http.request(req) }
   end
 end
