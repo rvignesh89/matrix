6cf066668b91d67632aa933a93a494786dd1c8db
rvignesh <rvignesh@outlook.com>
2015-11-13 00:22:04 +0800

    rollbaack debug logs

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -2,9 +2,8 @@ require 'date'
 
 class BookController < ApplicationController
   def all
-    #@books = Book.all()
-    @books = [{"name"=>"test"}]
-    #@books.sort! {|x,y| x['name'] <=> y['name']}
+    @books = Book.all()
+    @books.sort! {|x,y| x['name'] <=> y['name']}
   end
   def create
     Book.new({"name"=>params[:books][:name],"author"=>params[:books][:author]})
